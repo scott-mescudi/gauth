d41c6c2b48c79c5d31b02d6f4a61f388f9aac0de
script kiddie <anish.chittu2007@gmail.com>
2025-03-13 04:58:36 +0100

    added verified updaet password logic

---
+++ core/plain_auth/verifiedUpdatePasswordHandler.go
@@ -0,0 +1,90 @@
+package coreplainauth
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+	errs "github.com/scott-mescudi/gauth/shared/errors"
+	"github.com/scott-mescudi/gauth/shared/hashing"
+)
+
+func (s *Coreplainauth) VerifiedUpdatePassword(ctx context.Context, userID uuid.UUID, oldPassword, newPassword string) error {
+	if oldPassword == "" || newPassword == "" {
+		return errs.ErrEmptyCredentials
+	}
+
+	if oldPassword == newPassword {
+		return errs.ErrNoChange
+	}
+
+	if len(newPassword) > 254 {
+		return errs.ErrPasswordTooLong
+	}
+
+	if len(oldPassword) > 254 {
+		return errs.ErrPasswordTooLong
+	}
+
+	if oldPassword == newPassword {
+		return errs.ErrNoChange
+	}
+
+	oldPasswordHash, err := s.DB.GetUserPasswordByID(ctx, userID)
+	if err != nil {
+		return err
+	}
+
+	if ok, _ := hashing.ComparePassword(oldPassword, oldPasswordHash); !ok {
+		return errs.ErrIncorrectPassword
+	}
+
+	newPasswordHash, err := hashing.HashPassword(newPassword)
+	if err != nil {
+		return err
+	}
+
+	if oldPasswordHash == newPasswordHash {
+		return errs.ErrNoChange
+	}
+
+	token := uuid.New()
+
+	err = s.DB.SetUserVerificationDetails(ctx, userID, "update-password", newPassword, token.String(), 15*time.Minute)
+	if err != nil {
+		return err
+	}
+
+	email, err := s.DB.GetUserEmail(ctx, userID)
+	if err != nil {
+		return err
+	}
+
+	username, err := s.DB.GetUsername(ctx, userID)
+	if err != nil {
+		return err
+	}
+
+	return s.EmailProvider.SendEmail(email, username, s.Domain, token.String(), "update-password", s.EmailTemplateConfig.UpdatePasswordTemplate)
+}
+
+func (s *Coreplainauth) VerifyUpdatePassword(ctx context.Context, token string) error {
+	if token == "" {
+		return errs.ErrEmptyToken
+	}
+
+	vt, vi, userID, expiry, err := s.DB.GetUserVerificationDetails(ctx, token)
+	if err != nil {
+		return err
+	}
+
+	if time.Now().After(expiry) {
+		return errs.ErrInvalidToken
+	}
+
+	if vt != "update-password" {
+		return errs.ErrInvalidVerificationType
+	}
+
+	return s.DB.SetUserPassword(ctx, userID, vi)
+}
---
+++ core/plain_auth/verifiedUpdatePasswordHandler_test.go
@@ -0,0 +1,78 @@
+package coreplainauth
+
+import (
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/scott-mescudi/gauth/database"
+	"github.com/scott-mescudi/gauth/shared/auth"
+	"github.com/scott-mescudi/gauth/shared/email"
+	"github.com/scott-mescudi/gauth/shared/hashing"
+	tu "github.com/scott-mescudi/gauth/shared/testutils"
+)
+
+func TestVerifiedUpdatePassword(t *testing.T) {
+	str, clean, err := tu.SetupTestPostgresDBConnStr("")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer clean()
+
+	pool, err := database.ConnectToDatabase("postgres", str)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	hash, err := hashing.HashPassword("hey")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	uid, err := pool.AddUser(t.Context(), "", "", "jack", "jack@jack.com", "user", hash, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("valid update", func(t *testing.T) {
+		bldr := &strings.Builder{}
+		x := &auth.JWTConfig{Issuer: "jack", Secret: []byte("ljahdrfbdcvlj.hsbdflhb")}
+		pa := &Coreplainauth{
+			DB:                     pool,
+			AccessTokenExpiration:  1 * time.Hour,
+			RefreshTokenExpiration: 48 * time.Hour,
+			JWTConfig:              x,
+			EmailProvider:          &email.MockClient{Writer: bldr},
+			EmailTemplateConfig: &EmailTemplateConfig{
+				UpdateEmailTemplate:       "",
+				CancelUpdateEmailTemplate: "",
+				LoginTemplate:             "",
+				SignupTemplate:            "",
+				DeleteAccountTemplate:     "",
+				UpdatePasswordTemplate:    "",
+			},
+		}
+
+		err = pa.VerifiedUpdatePassword(t.Context(), uid, "hey", "heyq2")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		token := bldr.String()[:len("f895e4e1-620b-4914-979e-e6837676f461")]
+
+		err = pa.VerifyUpdatePassword(t.Context(), token)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		password, err := pool.GetUserPasswordByID(t.Context(), uid)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if password == hash {
+			t.Fatal("failed to update password")
+		}
+	})
+}
